14298d580397b96301179e60d648cb68831fa23d
gkinfotech <gokul.krish55@gmail.com>
2019-08-21 00:30:41 +0530

    Second Commit

---
+++ Java-Challenge/src/MathFunction.java
@@ -0,0 +1,10 @@
+public class MathFunction {
+    public static void main(String[] args) {
+        System.out.println(Math.random());
+        System.out.println("Maximum is:"+Math.max(12,5));
+        System.out.println("Minimum is:"+Math.min(2,1));
+        System.out.println("Multiplication of:"+Math.multiplyExact(5,2));
+        System.out.println("power number:"+Math.pow(2,2));
+        System.out.println("Square Root:"+Math.sqrt(2));
+    }
+}
---
+++ Java-Challenge/src/Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            int c = i * 5;
+            //System.out.println("["+i+"]"+ ["+ *+ "] + "5"+c);
+        }
+    }
+}
